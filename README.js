263f5ed54838057eeb7234cc6d6d6ab5c5bc489f
cse25 <cse25@users.noreply.github.com>
2015-08-22 13:16:07 -0500

    Update README.js

--- README.js
+++ README.js
@@ -1,28 +1,58 @@
-//Trivia Game
+//Country Capitals Trivia Game
 
-//Four objects with question-answer pairs.
+//Ten objects with question-answer pairs.
 var q1 = {
-    question: "What is the capital of Texas?",
-    answer: "AUSTIN"
+    question: "What is the capital of Kazakhstan?",
+    answer: "ASTANA"
 };
 
 var q2 = {
-    question: "What is the capital of New Jersey?",
-    answer: "TRENTON"
+    question: "What is the capital of Kenya?",
+    answer: "NAIROBI"
 };
 
 var q3 = {
-    question: "What is the capital of New York?",
-    answer: "ALBANY"
+    question: "What is the capital of the United Arab Emirates?",
+    answer: "ABU DHABI"
 };
 
 var q4 = {
-    question: "What is the capital of California?",
-    answer: "SACRAMENTO"
+    question: "What is the capital of Uruguay?",
+    answer: "MONTEVIDEO"
 };
 
-//An array of the objects.
-var questions = [q1,q2,q3,q4];
+var q5 = {
+    question: "What is the capital of Vietnam?",
+    answer: "HANOI"
+};
+
+var q6 = {
+    question: "What is the capital of Trinidad and Tobago?",
+    answer: "PORT OF SPAIN"
+};
+
+var q7 = {
+    question: "What is the capital of Switzerland?",
+    answer: "BERN"
+};
+
+var q8 = {
+    question: "What is the capital of Lebanon?",
+    answer: "BEIRUT"
+};
+
+var q9 = {
+    question: "What is the capital of Algeria?",
+    answer: "ALGIERS"
+};
+
+var q10 = {
+    question: "What is the capital of Argentina?",
+    answer: "BUENOS AIRES"
+};
+
+//An array of the question objects.
+var questions = [q1,q2,q3,q4,q5,q6,q7,q8,q9,q10];
 
 //Fisher-Yates Shuffle function applied to array.
 function shuffle(array) {
@@ -48,6 +78,7 @@ shuffle(questions);
 
 var score = 0;
 function questionOutput (questionarray) {
+    alert("Welcome to the Country Capitals Trivia Game!  There are 10 questions.  You have 5 seconds to answer each question.");
     for (var i = 0; i < questionarray.length; i++) {
         var start = new Date();
         var start2 = start.getTime();
@@ -66,7 +97,7 @@ function questionOutput (questionarray) {
             alert("Incorrect.")
         }
     }
-    if (score / questions.length >= 0.75){
+    if (score / questions.length >= 0.70){
     alert("You got " + score + " out of " + questions.length + " answers correct.  Good Job!");
     } else {
     alert("You got " + score + " out of " + questions.length + " answers correct.  Better luck next time.");
